e65fc13fbf669e3f7e3702934a7d1f5982e696a4
Vicky Ledsom <victorialedsom@gmail.com>
2017-03-17 16:22:40 +0000

    Add ajax weather forecast

--- views/thermostat_jquery.js
+++ views/thermostat_jquery.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
   var thermostat = new Thermostat();
   updateTemperature();
+  getWeather();
 
 $('#temperature-up').click(function(){
   thermostat.increase();
@@ -22,8 +23,16 @@ $('#power-saving').click(function(){
   $('#ps').text(thermostat.checkPowerSavingMode());
   updateTemperature();
 })
- function updateTemperature() {
-   $('#temperature').text(thermostat.checkCurrentTemperature());
 
+ function updateTemperature(){
+   $('#temperature').text(thermostat.checkCurrentTemperature());
  };
+
+ function getWeather(){
+   $.get("http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=15910454694d5f5407a95b7c04abe2a9&units=metric", function(data) {
+     $('#get-weather-desc').text(data.weather[0].description);
+     $('#get-weather-temp').text(data.main.temp + "°");
+     console.log(data);
+   })
+ }
 });
